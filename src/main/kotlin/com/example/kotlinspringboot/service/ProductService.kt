d0e49de9739094b8cc7fc616b983c95b8692bb3c
dawid <caushered@gmail.com>
2023-06-08 13:04:05 +0200

    feat: add logs

--- src/main/kotlin/com/example/kotlinspringboot/service/ProductService.kt
+++ src/main/kotlin/com/example/kotlinspringboot/service/ProductService.kt
@@ -5,8 +5,8 @@ import com.example.kotlinspringboot.dto.CreateProductResponseDTO
 import com.example.kotlinspringboot.dto.GetProductResponseDTO
 import com.example.kotlinspringboot.dto.GetProductsResponseDTO
 import com.example.kotlinspringboot.model.Product
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
-import org.springframework.http.HttpStatusCode
 import org.springframework.stereotype.Service
 import org.springframework.web.server.ResponseStatusException
 import java.time.LocalDateTime
@@ -15,6 +15,7 @@ import java.util.*
 @Service
 class ProductService {
     private val products: MutableList<Product> = mutableListOf()
+    private val log = LoggerFactory.getLogger(javaClass)
 
     fun getProducts(): GetProductsResponseDTO {
         return GetProductsResponseDTO(products);
@@ -25,6 +26,7 @@ class ProductService {
             val filteredProduct: Product = products.first { p -> p.id == productId }
             return GetProductResponseDTO(filteredProduct.id, filteredProduct.name, filteredProduct.price, filteredProduct.createdAt)
         } catch (e: NoSuchElementException) {
+            log.error("Product {} not found", productId)
             throw ResponseStatusException(HttpStatus.NOT_FOUND, "Product with id $productId not found")
         }
     }
@@ -32,7 +34,9 @@ class ProductService {
     fun createProduct(dto: CreateProductRequestDTO): CreateProductResponseDTO {
         val id: UUID = UUID.randomUUID()
         val createdAt = LocalDateTime.now()
-        products.add(Product(id, dto.name, dto.price, createdAt))
+        val product = Product(id, dto.name, dto.price, createdAt)
+        products.add(product)
+        log.info("Created product {}", product.id);
         return CreateProductResponseDTO(id, createdAt);
     }
 }
